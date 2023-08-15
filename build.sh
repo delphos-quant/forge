7f343629fb701774fd50e7faef0864c4c47949b1
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-15 05:05:46 -0300

    Updated build for linux

--- build.sh
+++ build.sh
@@ -16,12 +16,24 @@ create_executable() {
   fi
 }
 
+package() {
+  # Create the .sh launcher
+  echo '#!/bin/sh' > dist/strategy_manager.sh
+  echo './strategy_manager' >> dist/strategy_manager.sh
+  echo './strategy_manage.specr' >> dist/strategy_manager.spec
+  chmod +x dist/strategy_manager.sh
+
+  # Create the tarball
+  (
+    cd dist || { echo "Failed to change to dist directory"; exit 1; }
+    tar -czvf strategy_manager_linux.tar.gz strategy_manager strategy_manager.sh
+  )
+}
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   create_executable
-  echo '#!/bin/sh' > strategy_manager.sh
-  echo './dist/strategy_manager' >> strategy_manager.sh
-  chmod +x strategy_manager.sh
-  echo 'Build process completed! You can run the application using strategy_manager.sh.'
+  package
+  echo 'Build and packaging process completed! You can find the tarball in the dist directory.'
 else
   echo "OS $OSTYPE not supported"
   exit 1
