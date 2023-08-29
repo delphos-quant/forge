89c41d0a74ba0dedb3440736d3d2b348403a1e7b
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-29 01:12:21 -0300

    Added portfolio client instances

---
+++ portfolios/client1.py
@@ -0,0 +1,34 @@
+import websocket
+
+import dxlib as dx
+
+
+def on_message(ws, message):
+    print("Received Message:", message)
+
+
+def on_error(ws, error):
+    print("Error:", error)
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("WebSocket closed")
+
+
+def on_open(ws):
+    pass
+
+
+def main():
+    portfolio = dx.Portfolio().add_cash(1e4)
+    ws_url = "wss://localhost:6001/portfolio"  # replace with your WebSocket URL
+    ws = websocket.WebSocketApp(ws_url,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+
+    ws.send(portfolio.current_weights)
+
+
+if __name__ == "__main__":
+    main()
