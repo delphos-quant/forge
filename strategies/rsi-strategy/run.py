52bb7432d728bfaac52b1aacced8841bbaa2bdf8
Rafael Zimmer <rzimmerdev@gmail.com>
2023-09-06 23:50:12 -0300

    Updated strategies format

--- strategies/rsi-strategy/run.py
+++ strategies/rsi-strategy/run.py
@@ -7,12 +7,17 @@ from dxlib.strategies import RsiStrategy
 
 def main():
     logger = dx.info_logger()
-    server_port = int(os.environ["SERVER_PORT"])
+    server_port = int(os.environ["HTTP_PORT"])
     websocket_port = int(os.environ["WEBSOCKET_PORT"])
 
-    manager = StrategyManager(RsiStrategy(), server_port=server_port, websocket_port=websocket_port, logger=logger)
+    strategy = RsiStrategy(upper_bound=80, lower_bound=20)
 
+    manager = StrategyManager(strategy,
+                              server_port=server_port,
+                              websocket_port=websocket_port,
+                              logger=logger)
     manager.start()
+
     try:
         while manager.is_alive():
             with manager.server.exceptions as exceptions:
