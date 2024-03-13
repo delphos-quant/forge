3cd6ee17a4d2e5f1b512bae6a228a6e9ee669208
Rafael Zimmer <rzimmerdev@gmail.com>
2024-03-13 01:33:23 -0300

    Working strategy server

--- servers/strategies/rsi-strategy/run.py
+++ servers/strategies/rsi-strategy/run.py
@@ -1,5 +1,7 @@
+import asyncio
 import os
 import time
+from typing import AsyncGenerator
 
 import dxlib as dx
 from dxlib.interfaces.internal import MarketInterface
@@ -8,28 +10,47 @@ from dxlib.strategies.custom_strategies import RsiStrategy
 
 def main():
     logger = dx.InfoLogger()
-    interface_port = int(os.environ.get("INTERFACE_PORT", 8001))
+    interface_port = int(os.environ.get("INTERFACE_PORT", 4001))
 
     strategy = RsiStrategy(field="price")
-
-    interface = MarketInterface(interface_url=f"http://localhost:{interface_port}")
-    args = {
-        "tickers": ["AAPL", "MSFT"],
-        "start": "2024-02-01",
-        "end": "2024-02-28",
-    }
+    interface = MarketInterface(host="localhost")
 
     executor = dx.Executor(strategy)
-
+    ws = None
     try:
+        ws, quotes = interface.listen(interface.quote_stream, port=interface_port)
+
+        async def await_history():
+            return await quotes.__anext__()
+
+        history = asyncio.run(await_history())
+        schema = history.schema
+
+        async def bar_generator():
+            async for history in quotes:
+                history.schema = schema
+                for bar in history:
+                    yield bar
+
+        signals: AsyncGenerator = executor.run(bar_generator(), input_schema=schema)
+
+        # create thread to print signals
+        async def print_signals():
+            async for signal in signals:
+                print(signal)
+                # send ping
+
+        # thread
+        t = asyncio.run(print_signals())
+
         while True:
-            bars = interface.request(interface.quote, json=args)
-            signals = executor.run(bars)
-            print(signals)
-            time.sleep(60)
+            time.sleep(0.1)
+
     except KeyboardInterrupt:
         pass
     finally:
+        if ws:
+            ws.close()
         logger.info("Strategy manager has been shutdown.")
 
 
