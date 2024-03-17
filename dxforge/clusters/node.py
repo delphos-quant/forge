b3dbb4051e473efab3ac6bd7693580b54dbb7a93
Rafael Zimmer <rzimmerdev@gmail.com>
2024-03-17 19:02:35 -0300

    Added node and instance methods

--- dxforge/clusters/node.py
+++ dxforge/clusters/node.py
@@ -1,9 +1,11 @@
 import subprocess
 from dataclasses import dataclass
+from typing import Tuple
 
 import httpx
 from docker import DockerClient
 from docker.models.containers import Container
+from docker.models.images import Image
 
 
 def build(compose_file: str, service_name: str, docker):
@@ -17,129 +19,114 @@ def build(compose_file: str, service_name: str, docker):
 
 
 @dataclass
-class InstanceConfig:
+class NodeConfig:
     def __init__(self,
                  path: str,
                  tag: str,
-                 ports: dict[str, tuple | list | str],
+                 depends_on: list[str] = None,
+                 ports: dict[str, tuple | int] = None,
                  host: str = "localhost",
                  env: dict[str, str] = None):
         self.path = path
         self.tag = tag
-        self.ports = ports
+        self.depends_on = depends_on if depends_on else []
+        self.ports = ports if ports else {}
         self.host = host
         self.env = env
 
 
 class Instance:
     def __init__(self,
-                 config: InstanceConfig = None,
-                 docker_client: DockerClient = None
+                 config: NodeConfig = None,
                  ):
         self.config = config
-        self.docker_client = docker_client
+        self._container: Container | None = None
+        self._image: Image | None = None
 
-    def build(self):
-        return self.docker_client.images.build(
+    def build(self, docker_client: DockerClient) -> Image:
+        return docker_client.images.build(
             path=self.config.path,
             tag=self.config.tag
         )
 
-    def start(self, tag: str = None) -> Container:
-        return self.docker_client.containers.run(
+    def start(self, docker_client, tag: str = None) -> Container:
+        container = docker_client.containers.run(
             tag if tag else self.config.tag,
             ports={port: port for port in self.config.ports.values()},
             detach=True,
         )
+        self._container = container
+        return container
 
-    def stop(self, container: Container):
-        container.stop()
-        container.remove()
+    def stop(self):
+        if self._container:
+            self._container.stop()
+            self._container.remove()
+            self._container = None
+
+    @property
+    def alive(self):
+        if not self._container:
+            return False
+        return self._container.status == "running"
 
 
 class Node:
-    def __init__(self, image, ports: dict[str, tuple | list | str], host=None, docker: DockerClient = None):
-        self.image = image
-        self.container: Container | None = None
-
-        for name, port in ports.items():
-            if isinstance(port, str):
-                ports[name] = port
-
-        self.ports: dict[str, tuple | list | str] = ports
-        self.host = host if host else "localhost"
-        self.docker = docker
+    def __init__(self, config: NodeConfig, docker_client: DockerClient = None):
+        self._config = config
+        self.docker_client = docker_client
+        self.instances = {}
 
     @classmethod
-    def from_file(cls, image, docker: DockerClient):
-        image_config = image.attrs['Config']
+    def from_dict(cls, config: dict, docker_client: DockerClient):
+        config = NodeConfig(
+            path=config.get("path"),
+            tag=config.get("tag"),
+            depends_on=config.get("depends_on"),
+            ports=config.get("ports"),
+            host=config.get("host"),
+            env=config.get("env")
+        )
 
-        env_vars = image_config.get('Env', [])
-        env = {}
-        for var in env_vars:
-            key, value = var.split('=', 1)
-            env[key] = value
-
-        http_port = env.get('HTTP_PORT', None)
-        websocket_port = env.get('WEBSOCKET_PORT', None)
-
-        if not http_port and not websocket_port:
-            return
-
-        ports = {}
-
-        if http_port:
-            ports['HTTP_PORT'] = str(http_port)
-        if websocket_port:
-            ports['WEBSOCKET_PORT'] = str(websocket_port)
-
-        host = env.get('HOST', None)
-
-        service = cls(image, ports, host, docker=docker)
-        return service
+        return cls(config, docker_client)
 
     @property
     def client(self):
         return httpx.AsyncClient()
 
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def alive(self):
+        return any([instance.alive for instance in self.instances.values()])
+
+    def add_instance(self, uuid: str):
+        self.instances[uuid] = Instance(self._config)
+
+    def build(self):
+        if self.docker_client:
+            for instance in self.instances.values():
+                instance.build(self.docker_client)
+        else:
+            raise NotImplementedError("No script manager implemented")
+
     def start(self):
-        if self.docker:
-            self.container = self.docker.containers.run(
-                self.image,
-                ports={port: port for port in self.ports.values()},
-                detach=True,
-            )
+        if self.docker_client:
+            for instance in self.instances.values():
+                instance.start(self.docker_client)
         else:
             raise NotImplementedError("No script manager implemented")
 
     def stop(self):
-        if self.docker and self.container:
-            self.container.stop()
-            self.container.remove()
+        if self.docker_client:
+            for instance in self.instances.values():
+                instance.stop()
         else:
             raise NotImplementedError("No script manager implemented")
 
-    async def get(self, endpoint: str):
-        if not self.docker:
-            raise NotImplementedError("No script manager implemented")
-        elif not self.container:
-            raise RuntimeError("Container not started")
-
-        protocol = "http"
-        async with self.client:
-            return await self.client.get(
-                f"{protocol}://{self.host}:{self.ports['HTTP_PORT']}{endpoint}"
-            )
-
-    async def post(self, endpoint: str, data):
-        if not self.docker:
-            raise NotImplementedError("No script manager implemented")
-        elif not self.container:
-            raise RuntimeError("Container not started")
-
-        protocol = "http"
-        async with self.client:
-            return await self.client.post(
-                f"{protocol}://{self.host}:{self.ports['HTTP_PORT']}{endpoint}",
-                json=data
-            )
+    def get_interface(self, name=None) -> Tuple[str, int]:
+        if name is None:
+            raise NotImplementedError("No instance union implemented")
+        return self._config.host, self._config.ports[name]
